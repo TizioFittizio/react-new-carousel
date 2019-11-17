aba9ba25abb6b9dc19f86aad1449b7b4b700e8f5
Luca Campana <luca.campa.94@gmail.com>
2019-11-17 21:35:11 +0100

    First carousel

---
+++ lib/Carousel.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+
+interface Props {
+
+}
+
+interface State {
+
+}
+
+export class Carousel extends React.Component<Props, State> {
+
+    render(){
+        return (
+            <div>TODO</div>
+        );
+    }
+
+}
